4a8ed111aee4d13c960c867d569cb7daf1dd790f
cyrilcon <constantine.sonic@gmail.com>
2025-04-11 11:08:06 +0500

    Support sending notifications to multiple recipients

--- api/api_v1/crud/notification.py
+++ api/api_v1/crud/notification.py
@@ -1,47 +1,46 @@
 from datetime import datetime, timezone
-from typing import Tuple
+from typing import List
 
 from sqlalchemy.ext.asyncio import AsyncSession
 
-from api.api_v1.models import NotificationRequest
 from database.models import Document, Notification
 
 
 async def create_notification(
     session: AsyncSession,
-    notification_in: NotificationRequest,
-) -> Tuple[Notification | None, str | None]:
+    chat_id: int,
+    message: str,
+    button_url: str | None = None,
+    documents: List[Document] | None = None,
+) -> Notification:
     """
-    Create a new notification and its associated documents in the database.
+    Create a notification in database.
 
-    :param session: Session for working with the database.
-    :param notification_in: Body of the notification request.
-    :return: The notification object and None if successful, otherwise None and an error message.
+    :param session: Async database session.
+    :param chat_id: Telegram chat id.
+    :param message: Text to send.
+    :param button_url: Optional URL for an inline button in the message.
+    :param documents: List of files to send.
+    :return: Notification object.
     """
-    try:
-        notification = Notification(
-            chat_id=notification_in.chatID,
-            message=notification_in.message,
-            button_url=notification_in.buttonUrl,
-            created_at=datetime.now(timezone.utc),
-        )
+    notification = Notification(
+        chat_id=chat_id,
+        message=message,
+        button_url=button_url,
+        created_at=datetime.now(timezone.utc),
+    )
 
-        session.add(notification)
-        await session.flush()
+    session.add(notification)
+    await session.flush()
 
-        documents = notification_in.documents
-        if documents:
-            for document in documents:
-                new_document = Document(
-                    notification_id=notification.id,
-                    buffer=document.buffer,
-                    name=document.name,
-                )
-                session.add(new_document)
+    if documents:
+        for document in documents:
+            new_document = Document(
+                notification_id=notification.id,
+                buffer=document.buffer,
+                name=document.name,
+            )
+            session.add(new_document)
 
-        await session.commit()
-        return notification, None
-
-    except Exception as e:
-        await session.rollback()
-        return None, str(e)
+    await session.commit()
+    return notification
--- api/api_v1/models/notification.py
+++ api/api_v1/models/notification.py
@@ -1,4 +1,5 @@
 from datetime import datetime
+from typing import List
 
 from pydantic import BaseModel, Field
 
@@ -6,22 +7,22 @@ from .document import Document
 
 
 class NotificationRequest(BaseModel):
-    chatID: int = Field(
+    chatIds: List[int] = Field(
         ...,
-        description="ID of the chat or channel where the message will be sent",
-        examples=[123456789],
+        description="List of chat or channel IDs where the message will be sent",
+        examples=[[123456789, 987654321]],
     )
     message: str = Field(
         ...,
-        description="Message body in Markdown_v2 format",
-        examples=["Hello, this is a message with *markdown* formatting."],
+        description="Message body in MarkdownV2 format",
+        examples=["Hello, this is a message with *markdown* formatting"],
     )
     buttonUrl: str | None = Field(
         None,
         description="Optional URL for an inline button in the message. If provided, the message will include a button linking to this URL.",
         examples=["https://example.com"],
     )
-    documents: list[Document] | None = Field(
+    documents: List[Document] | None = Field(
         None,
         description="Optional list of attached documents",
         examples=[
@@ -33,17 +34,7 @@ class NotificationRequest(BaseModel):
     )
 
 
-class NotificationResponse(BaseModel):
-    success: bool = Field(
-        ...,
-        description="True if the message was sent successfully",
-        examples=[True],
-    )
-    errorMessage: str | None = Field(
-        None,
-        description="Error message if something went wrong, otherwise null",
-        examples=[None],
-    )
+class NotificationResponse(NotificationRequest):
     createdAt: datetime = Field(
         ...,
         description="Time of notification sending in ISO 8601 format",
